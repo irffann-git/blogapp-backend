1898474db482f7e5285054138880d2ee5439013d
irffann-git <rxmeirfan7@gmail.com>
2026-05-25 16:43:08 +0530

    connect front end 8

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,24 +1,14 @@
 const express = require("express");
-
-const {
-  addComment,
-  getComments,
-} = require("../controllers/commentController");
-
-const { protect } =
-  require("../middleware/authMiddleware");
+const { addComment, getComments } = require("../controllers/commentController");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post(
-  "/:blogId",
-  protect,
-  addComment
-);
+router.post("/:blogId", protect, addComment);
+router.get("/:blogId", getComments);
 
-router.get(
-  "/:blogId",
-  getComments
-);
+router.get("/test", (req, res) => {
+  res.json({ message: "Comment routes are working" });
+});
 
-module.exports = router;
+module.exports = router; // ✅ must be present
