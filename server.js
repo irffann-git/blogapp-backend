0437afc16317c546c0c3a72f1abdf47d9cb7f4b9
irffann-git <rxmeirfan7@gmail.com>
2026-05-25 12:57:11 +0530

    connect front end

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(express.json());
+
+app.use(cors({
+  origin: "https://blogappirf.vercel.app/",
+  credentials: true
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/blogs", blogRoutes);
