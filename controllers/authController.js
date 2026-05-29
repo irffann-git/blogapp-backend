2c089cd9f0f877f43e37f83018cbf4832079242f
irffann-git <rxmeirfan7@gmail.com>
2026-05-29 15:23:12 +0530

    comment like option added 46

--- controllers/authController.js
+++ controllers/authController.js
@@ -97,11 +97,41 @@ const getProfile = async (req, res) => {
 };
 
 
+const updateProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+
 
 
 
 module.exports = {
   registerUser,
   loginUser,
-  getProfile
+  getProfile,
+  updateProfile,
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { registerUser,loginUser,getProfile } = require("../controllers/authController");
+const { registerUser,loginUser,getProfile,updateProfile } = require("../controllers/authController");
 
 
 const { protect } = require("../middleware/authMiddleware");
